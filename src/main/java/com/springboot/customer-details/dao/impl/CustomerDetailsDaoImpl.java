85b32dd77939a2ebbd74c8aac7d3cde2f5ee6b0a
sohailpasha1 <157475077+sohailpasha1@users.noreply.github.com>
2024-02-05 18:48:05 +0530

    Create CustomerDetailsDaoImpl.java

---
+++ src/main/java/com/springboot/customer-details/dao/impl/CustomerDetailsDaoImpl.java
@@ -0,0 +1,10 @@
+package com.customerdetails.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import com.customerdetails.dao.CustomerDetailsDao;
+
+@Repository
+public class CustomerDetailsDaoImpl extends CommonDAOImpl implements CustomerDetailsDao {
+
+}
